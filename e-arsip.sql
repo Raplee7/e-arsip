307bbb780884370bb2cafef3efd51fe4e9b92a42
12200964-rafli <kehidupanrafli@gmail.com>
2021-12-13 15:52:29 +0700

    database sql

---
+++ e-arsip.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 13, 2021 at 06:15 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `e-arsip`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_arsip`
+--
+
+CREATE TABLE `tbl_arsip` (
+  `id_arsip` int(11) NOT NULL,
+  `id_kategori` int(11) DEFAULT NULL,
+  `no_arsip` varchar(10) DEFAULT NULL,
+  `nama_file` varchar(255) DEFAULT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `tgl_upload` date DEFAULT NULL,
+  `tgl_update` date DEFAULT NULL,
+  `file_arsip` varchar(255) DEFAULT NULL,
+  `id_depatemen` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_departemen`
+--
+
+CREATE TABLE `tbl_departemen` (
+  `id_dep` int(11) NOT NULL,
+  `nama_dep` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(11) NOT NULL,
+  `nama_user` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `level` int(1) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `id_departemen` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `nama_user`, `email`, `password`, `level`, `foto`, `id_departemen`) VALUES
+(1, 'Rafli Marian Mirza', 'raflimarianm@gmail.com', '12200964', 1, 'user1.png', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_arsip`
+--
+ALTER TABLE `tbl_arsip`
+  ADD PRIMARY KEY (`id_arsip`);
+
+--
+-- Indexes for table `tbl_departemen`
+--
+ALTER TABLE `tbl_departemen`
+  ADD PRIMARY KEY (`id_dep`);
+
+--
+-- Indexes for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_arsip`
+--
+ALTER TABLE `tbl_arsip`
+  MODIFY `id_arsip` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_departemen`
+--
+ALTER TABLE `tbl_departemen`
+  MODIFY `id_dep` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
