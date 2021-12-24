afccda7ae5c8029159cae150e4fb82955b5404e6
12200964-rafli <kehidupanrafli@gmail.com>
2021-12-24 16:19:22 +0700

    Database

--- e-arsip.sql
+++ e-arsip.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 23, 2021 at 09:27 AM
+-- Generation Time: Dec 24, 2021 at 10:18 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `e-arsip-ci4`
+-- Database: `e-arsip`
 --
 
 -- --------------------------------------------------------
@@ -46,9 +46,7 @@ CREATE TABLE `tbl_arsip` (
 --
 
 INSERT INTO `tbl_arsip` (`id_arsip`, `id_kategori`, `no_arsip`, `nama_arsip`, `deskripsi`, `tgl_upload`, `tgl_update`, `file_arsip`, `ukuran_file`, `id_dep`, `id_user`) VALUES
-(7, 12, '20100259-sjFJ', 'Sertifikat seminar', NULL, '2020-10-02', '2020-10-02', '1601643438_f6c983f776afce130031.pdf', 1000, 1, 1),
-(8, 3, '20100228-PtZ5', 'Surat Keputusan', 'Surat Keputusan', '2020-10-02', '2020-10-02', '1601643665_70cb49d74ee3cdf2ffc0.pdf', 632879, 1, 1),
-(9, 2, '20100219-owBC', 'Surat Keluar Ke Kantor Camat', 'Surat Keluar Ke Kantor Camat', '2020-10-02', '2020-10-02', '1601644005_e6dea022fdbf6549db8e.pdf', 632879, 1, 1);
+(11, 12, '211224 - FV8Q', 'Sertifikat Vaksin', 'Sertifikat vaksin pertama Raflee', '2021-12-24', '2021-12-24', '1640333241_2b53153f0d3276c6fef6.pdf', 158948, NULL, 11);
 
 -- --------------------------------------------------------
 
@@ -88,11 +86,6 @@ CREATE TABLE `tbl_kategori` (
 INSERT INTO `tbl_kategori` (`id_kategori`, `nama_kategori`) VALUES
 (1, 'Surat Masuk'),
 (2, 'Surat Keluar'),
-(3, 'Berkas Kerja'),
-(4, 'Surat Keputusan'),
-(5, 'Arsip Umum'),
-(6, 'Berkas Kesehatan'),
-(7, 'Berkas Internal'),
 (12, 'Sertifikat');
 
 -- --------------------------------------------------------
@@ -106,6 +99,7 @@ CREATE TABLE `tbl_user` (
   `nama_user` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
+  `level` int(1) DEFAULT NULL,
   `foto` varchar(255) DEFAULT NULL,
   `id_dep` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
@@ -114,11 +108,14 @@ CREATE TABLE `tbl_user` (
 -- Dumping data for table `tbl_user`
 --
 
-INSERT INTO `tbl_user` (`id_user`, `nama_user`, `email`, `password`, `foto`, `id_dep`) VALUES
-(1, 'Rafli Marian Mirza', 'raflimarianm@gmail.com', '1234', 'user1.jpg', 1),
-(2, 'Budi', 'budi@gmail.com', '1234', 'User.png', 2),
-(3, 'Andi', 'andi@gmail.com', '1234', 'User.png', 3),
-(8, 'Ani', 'ani@gmail.com', '1234', '1601116917_ba4a3e3fd9841cf88fd3.png', 3);
+INSERT INTO `tbl_user` (`id_user`, `nama_user`, `email`, `password`, `level`, `foto`, `id_dep`) VALUES
+(11, 'Rafli Marian Mirza', 'raflimarianm@gmail.com', '1234', 1, '1640330712_d5770452d89f6345b191.jpg', 1),
+(12, 'Ilfar Nairam Azrim', 'ilfar@gmail.com', '1234', 2, '1640333698_186317fc2cefb9f7d982.jpg', 1),
+(13, 'Dany Alhady', 'dany@gmail.com', '1234', 1, '1640336827_01e80e5672b6d34c4f82.png', 1),
+(14, 'Teguh Prawira', 'teguh@gmail.com', '1234', 1, '1640336807_afc32a657d9e5f77f0bf.png', 2),
+(15, 'Saiful Veri', 'saiful@gmai.com', '1234', 1, '1640336863_7db9f548335d3688807a.png', 2),
+(16, 'Realdy Wiraputra', 'realdy@gmail.com', '1234', 1, '1640336894_0654726a51b7afa79688.jpg', 3),
+(17, 'Aldy Tri Wahyudi', 'aldy@gmail.com', '1234', 1, '1640336931_5ca10a8146dc9bf5aa48.png', 3);
 
 --
 -- Indexes for dumped tables
@@ -168,13 +165,13 @@ ALTER TABLE `tbl_dep`
 -- AUTO_INCREMENT for table `tbl_kategori`
 --
 ALTER TABLE `tbl_kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
